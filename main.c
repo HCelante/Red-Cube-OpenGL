e0abdb9f849e7bcb70605e166f811deec183f73e
HCelante <higor.celante@gmail.com>
2019-09-09 16:57:41 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,174 +1,112 @@
-
-#include <GL/glut.h>
 #include <stdio.h>
-// Função callback chamada para fazer o desenho
-int tx = 0;
-int ty = 0;
-int tz = 0;
-int scale = 0;
-int rx = 0;
-int ry = 0;
-int rz = 0;
-char action = 'n';
-GLfloat spin = 0;
-void quadrado(void){
-    glBegin(GL_QUADS);
-               glVertex2i(0,15);
-               glVertex2i(0,0);
-               // Especifica que a cor corrente é azul
-               //glColor3f(0.0f, 0.0f, 1.0f);
-               glVertex2i(15,0);
-               glVertex2i(15,15);               
-     glEnd();
-}
-void Desenha(void)
+#include <GL/glut.h>
+
+char comando = 't';
+double translacaoX = 0, translacaoY = 0, rotacaoX = 10, rotacaoY = 0, key;
+// funcao de inicializacao
+int init()
 {
-
-    glClear(GL_COLOR_BUFFER_BIT);
-     //glMatrixMode(GL_MODELVIEW);
-    glPushMatrix(); 
-     //glLoadIdentity();
-
-     glColor3f(1.0f, 0.0f, 0.0f);
-
-
-    quadrado();
-
-    glPopMatrix();
-     // Desenha um quadrado preenchido com a cor corrente
-     
-     // Executa os comandos OpenGL
-     glFlush();
-    glutSwapBuffers();
-
+   glClearColor(0, 0, 0, 0);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentitY();
+   gluOrtho2D(0, 100, 0, 100);
 }
 
-// Inicializa parâmetros de rendering
-void Inicializa (void)
-{   
-    // Define a cor de fundo da janela de visualização como preta
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-}
-
-// Função callback chamada quando o tamanho da janela é alterado 
-void AlteraTamanhoJanela(GLsizei w, GLsizei h)
+void display()
 {
-                   // Evita a divisao por zero
-                   if(h == 0) h = 1;
-                           
-                   // Especifica as dimensões da Viewport
-                   glViewport(0, 0, w, h);
-
-                   // Inicializa o sistema de coordenadas
-                   glMatrixMode(GL_PROJECTION);
-                   glLoadIdentity();
-
-                   // Estabelece a janela de seleção (left, right, bottom, top)
-                   if (w <= h) 
-                           gluOrtho2D (0.0f, 100.0f, 0.0f, 100.0f*h/w);
-                   else 
-                           gluOrtho2D (0.0f, 100.0f*w/h, 0.0f, 100.0f);
+   glClear(GL_COLOR_BUFFER_BIT);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentitY();
+   glTranslatef(translacaoX, translacaoY, 0); // translada para a posicao alterada
+   glTranslatef(5, 5, 0);
+   glRotatef(rotacaoY, 0.0, 0.0, 1);
+   glTranslatef(-5, -5, 0);
+   glBegin(GL_POLYGON); // desenha o quadrado
+   glColor3f(1, 0, 0);
+   glVertex2i(0, 0);
+   glColor3f(1, 0, 0);
+   glVertex2i(10, 0);
+   glColor3f(1, 0, 0);
+   glVertex2i(10, 10);
+   glColor3f(1, 0, 0);
+   glVertex2i(0, 10);
+   glEnd();
+   glFlush();
 }
-
-// Programa Principal 
-void direcional(int key, int x, int y){
-     switch (key)
-  {
-    case  GLUT_KEY_UP:
-        printf("up");
-       if (action == 'r')
-          ry+= 1;
-          glMatrixMode(GL_PROJECTION);
-          glLoadIdentity();
-          glRotatef(spin,rx,ry,rz);
-
-       if (action == 't')
-          ty+= 1;
-           glMatrixMode(GL_PROJECTION);
-           glLoadIdentity();
-           glTranslatef(tx,ty,tz);
-
-    case  GLUT_KEY_DOWN:
-       if (action == 'r')
-          ry-= 1;
-          glMatrixMode(GL_PROJECTION);
-          glLoadIdentity();
-          glRotatef(spin,rx,ry,rz);
-       if (action == 't')
-          ty-= 1;
-           glMatrixMode(GL_PROJECTION);
-           glLoadIdentity();
-           glTranslatef(tx,ty,tz);
-    case  GLUT_KEY_LEFT:
-       if (action == 'r')
-          rx-= 1;
-          glMatrixMode(GL_PROJECTION);
-          glLoadIdentity();
-          glRotatef(spin,rx,ry,rz);
-       if (action == 't')
-          tx-= 1;
-           glMatrixMode(GL_PROJECTION);
-           glLoadIdentity();
-           glTranslatef(tx,ty,tz);
-    case  GLUT_KEY_RIGHT:
-       if (action == 'r')
-          rx+= 1;
-          glMatrixMode(GL_PROJECTION);
-          glLoadIdentity();
-          glRotatef(spin,rx,ry,rz);
-       if (action == 't')
-          tx+= 1;
-           glMatrixMode(GL_PROJECTION);
-           glLoadIdentity();
-           glTranslatef(tx,ty,tz);
-    }
-    glutPostRedisplay();    
-}
- 
-void Keyboard(unsigned char key, int x, int y)
+void teclasChar(unsigned char key, int x, int y)
 {
-  switch (key)
-  {
-  case 27:             // ESCAPE key
-	  exit (0);
-	  break;
-
-  case 'r':
-      printf("rotate");
-      action = 'r';
-	  //rotate();
-	  break;
-
-  case 't':
-      action = 't';
-	  //translate();
-	  break;
-
-  case 'v':
-      action = 'v';
-	  //velocidade();
-	  break;
-  
-  }
+   switch (key)
+   {
+   case 'R':
+   case 'r':
+      comando = 'r';
+      break;
+   case 'T':
+   case 't':
+      comando = 't';
+      break;
+   }
+   glutPostRedisplay();
 }
 
-
-
-int main(int argc, char** argv)
+void teclasEspeciais(int key)
 {
-     glutInit(&argc, argv);
-     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-     glutInitWindowSize(500,500);
-     glutInitWindowPosition(10,10);
-     glutCreateWindow("Quadrado");
+   if (comando == 't')
+   {
+      if (key == GLUT_KEY_DOWN)
+      {
+         translacaoY -= 1;
+      }
+      else if (key == GLUT_KEY_LEFT)
+      {
+         translacaoX -= 1;
+      }
+      else if (key == GLUT_KEY_UP)
+      {
+         translacaoY += 1;
+      }
+      else if (key == GLUT_KEY_RIGHT)
+      {
+         translacaoX += 1;
+      }
 
-     
-     glutDisplayFunc(Desenha);
-     glutReshapeFunc(AlteraTamanhoJanela);
-     glutSpecialFunc(direcional);
-     glutKeyboardFunc (Keyboard);
-     
-     
-    Inicializa();
-     glutMainLoop();
+   }
+   else if (comando == 'r')
+   {
+      if (key == GLUT_KEY_DOWN)
+      {
+         rotacaoY -= rotacaoX;
+      }
+      else if (key == GLUT_KEY_RIGHT)
+      {
+         rotacaoY -= rotacaoX;
+      }
+      else if (key == GLUT_KEY_LEFT)
+      {
+         rotacaoY += rotacaoX;
+      }
+      else if (key == GLUT_KEY_UP)
+      {
+         rotacaoY += rotacaoX;
+      }
+
+
+   }
+   glutPostRedisplay();
+}
+
+int main(int argc, char **argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+   glutInitWindowSize(500, 500);
+   //glutInitWindowPosition(600, 0);
+
+   glutCreateWindow("Atividade 1 \n- Por Higor Celante");
+   glutDisplayFunc(display);
+   init();
+   glutteclasChar(teclasChar);
+   glutSpecialFunc(teclasEspeciais);
+   glutMainLoop();
 }
